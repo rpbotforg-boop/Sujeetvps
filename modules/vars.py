3fcc1af28651b9a84e6e49f2f455dc2ea95c9c12
rpbotforg-boop <rpbotforg@gmail.com>
2026-01-06 22:23:42 +0530

    Change bot_token in vars.py
    Updated bot_token for security reasons.

--- modules/vars.py
+++ modules/vars.py
@@ -1,5 +1,6 @@
 api_id = "22447622"
 api_hash = "543b62d58d3e723e766ba57a984ab65d"
-bot_token = "8225118430:AAEOFTRcVH3Au1LR0iFyasUb4U5CGATuoT4"
+bot_token = "8387440827:AAFuE8cD6GS7ShNwovdRCGLc49x59gD1Ufo"
+
 
 
